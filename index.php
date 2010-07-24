86027d847efde2d7877d831180348b1b4c2c5661
Xan Manning <xan.manning@gmail.com>
2010-07-24 18:43:01 +0100

    Updated to v1.0.4

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 
 /*
- *	Knoxious Open Pastebin		 v 1.0.2
+ *	Knoxious Open Pastebin		 v 1.0.4
  * ============================================================================
  *	
  *	Copyright (c) 2009-2010 Xan Manning (http://xan-manning.co.uk/)

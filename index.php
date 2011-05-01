f3e8b9f12e112590ffcd1beaa36ef1b79621fce3
Xan Manning <xan.manning@gmail.com>
2011-05-01 23:33:03 +0100

    Now on version 1.1.11, updated README with features. Some more fixes on the way in version 1.1.12...

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 
 /*
- *	Knoxious Open Pastebin		 v 1.1.10
+ *	Knoxious Open Pastebin		 v 1.1.11
  * ============================================================================
  *	
  *	Copyright (c) 2009-2010 Xan Manning (http://xan-manning.co.uk/)
